86c91e3eefede27ad9e2a5bdb17d54501c1cf72c
Shreya <prakashshreya093@gmail.com>
2026-04-12 01:13:03 +0530

    Add files via upload

---
+++ 17.R
@@ -0,0 +1,10 @@
+students <- data.frame( Name =c("Aman","Riya","Karan","Neha","Arjun"),
+Age = c(20,21,19,22,20), 
+Height = c(170,165,175,160,172),
+Weight = c(65,55,70,50,68) ) 
+print("Student Data Frame:") 
+print(students)
+print("Number of rows:") 
+nrow(students) 
+print("Number of columns:") 
+ncol(students)
