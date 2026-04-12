9f3cd36076f1cccd55d38d3e10393cbee9c9fff8
Shreya <prakashshreya093@gmail.com>
2026-04-12 21:09:13 +0530

    Add files via upload

---
+++ 26.R
@@ -0,0 +1,9 @@
+# Using seq() to generate sequence
+seq1 <- seq(1, 10, by = 2)
+print(seq1)
+# Using rep() to repeat values
+rep1 <- rep(5, times = 4)
+print(rep1)
+# Repeating a sequence
+rep2 <- rep(c(1,2,3), times = 3)
+print(rep2)
